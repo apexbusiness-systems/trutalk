b305917b2fe79e3cf3fcdef3e675421829951cbb
TradeLine 24/7 <jrmendozaceo@apexbusiness-systems.com>
2026-01-18 00:28:03 -0700

    fix(mobile): Correct eslint disable rule name in metro.config.js

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 const { getDefaultConfig } = require('@expo/metro-config');
 const path = require('path');
